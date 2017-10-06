722d498424589f33f636c93c43cde82c080c03df
Templum <simonpelczer@outlook.de>
2017-10-06 08:57:11 +0200

    :sparkles: Added Async Section

---
+++ 6_Async/Instructions.md
@@ -0,0 +1,19 @@
+# Async
+
+## General
+
+This project shows ways for asynchronous code execution. Also, it shows the different options callbacks, promises and async/await. Further, it gives a glimpse of the so-called "callback hell".
+
+![Callback Hell Sample Image](https://collinmakersquare.files.wordpress.com/2016/02/b4uajfmcqae67qb.png?w=1000 "Callback Sample Image")
+
+## Usages
+
+This project contains none dependencies, therefore you are able to directly execute this sample using `$ yarn start`.
+Afterwards, you should see a folder `downloads containing 3 JSON files and additional you should see the following console output:
+
+```Code
+Request failed
+Request failed
+Request failed
+✨  Done in X.XXs.
+```
---
+++ 6_Async/index.js
@@ -0,0 +1,66 @@
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+
+const writeFile = promisify(fs.writeFile);
+
+function request(url, succeed = true, callback = null) {
+    const TIME = 4000;
+    if (callback) {
+        setTimeout(() => {
+            callback(succeed ? null : new Error('Request failed'), { success: true, status: 200, body: 'Hello World' });
+        }, TIME);
+        return;
+    } else {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (succeed) {
+                    resolve({ success: true, status: 200, body: 'Hello World' });
+                } else {
+                    reject(new Error('Request failed'));
+                }
+            }, TIME);
+        });
+    }
+}
+
+function callbackFlow() {
+    request('special-page', true, (err, res) => {
+        if (err) return console.error(err.message);
+        fs.writeFile(path.join(__dirname, 'downloads', 'cb-special-page.json'), JSON.stringify(res), (err, res) => {
+            if (err) return console.error(err.message);
+            request('fail-page', false, (err, res) => {
+                if (err) return console.error(err.message);
+                fs.writeFile(path.join(__dirname, 'downloads', 'cb-fail-page.json'), JSON.stringify(res), (err, res) => {
+                    if (err) return console.error(err.message);
+                });
+            });
+        });
+    });
+}
+
+function promiseFlow() {
+    request('special-page', true)
+        .then(res => writeFile(path.join(__dirname, 'downloads', 'promise-special-page.json'), JSON.stringify(res)), err => { throw err })
+        .then(_ => request('fail-page', false), err => { throw err })
+        .then(res => writeFile(path.join(__dirname, 'downloads', 'promise-fail-page.json'), JSON.stringify(res)), err => { throw err })
+        .catch(err => console.error(err.message));
+}
+
+async function asyncFlow() {
+    try {
+        let specialPage = await request('special-page', true);
+        await writeFile(path.join(__dirname, 'downloads', 'await-special-page.json'), JSON.stringify(specialPage));
+        let failPage = await request('fail-page', false);
+        await writeFile(path.join(__dirname, 'downloads', 'await-fail-page.json'), JSON.stringify(failPage));
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
+
+if (!fs.existsSync(path.join(__dirname, 'downloads'))) fs.mkdirSync(path.join(__dirname, 'downloads'))
+
+callbackFlow();
+promiseFlow();
+asyncFlow();
